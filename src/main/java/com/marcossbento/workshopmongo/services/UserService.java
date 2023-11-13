a7af1c182949f289adb236e8e851b56fd2b6f7f3
marcossbento <marcossbento14@gmail.com>
2023-11-13 20:16:36 -0300

    Connecting MongoDB with repository and service

---
+++ src/main/java/com/marcossbento/workshopmongo/services/UserService.java
@@ -0,0 +1,19 @@
+package com.marcossbento.workshopmongo.services;
+
+import com.marcossbento.workshopmongo.domain.User;
+import com.marcossbento.workshopmongo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repo;
+
+    public List<User> findAll() {
+        return repo.findAll();
+    }
+}
