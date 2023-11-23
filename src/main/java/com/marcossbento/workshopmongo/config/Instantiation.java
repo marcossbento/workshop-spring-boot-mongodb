ef5d7fea759c61ff5a1fa6c4772a116459c9c4bb
marcossbento <marcossbento14@gmail.com>
2023-11-23 19:56:36 -0300

    Adding Comments to posts with DTO

--- src/main/java/com/marcossbento/workshopmongo/config/Instantiation.java
+++ src/main/java/com/marcossbento/workshopmongo/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.marcossbento.workshopmongo.config;
 import com.marcossbento.workshopmongo.domain.Post;
 import com.marcossbento.workshopmongo.domain.User;
 import com.marcossbento.workshopmongo.dto.AuthorDTO;
+import com.marcossbento.workshopmongo.dto.CommentDTO;
 import com.marcossbento.workshopmongo.repository.PostRepository;
 import com.marcossbento.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,6 +40,13 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viageeem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().addAll(Arrays.asList(c3));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
