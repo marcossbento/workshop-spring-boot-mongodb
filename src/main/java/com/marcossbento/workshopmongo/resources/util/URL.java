a5ecc7cac7162dd9f4a23c1b97e1417fc3e0d99e
marcossbento <marcossbento14@gmail.com>
2023-11-27 18:58:12 -0300

    Multi-criteria consultation

--- src/main/java/com/marcossbento/workshopmongo/resources/util/URL.java
+++ src/main/java/com/marcossbento/workshopmongo/resources/util/URL.java
@@ -2,7 +2,9 @@ package com.marcossbento.workshopmongo.resources.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
-
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 
 public class URL {
@@ -12,6 +14,16 @@ public class URL {
                 return URLDecoder.decode(text, "UTF-8");
             } catch (UnsupportedEncodingException e) {
                 return "";
+            }
+        }
+
+        public static Date convertDate(String textDate, Date defaultValue) {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+            try {
+                return sdf.parse(textDate);
+            } catch (Exception e) {
+                return defaultValue;
             }
         }
 }
